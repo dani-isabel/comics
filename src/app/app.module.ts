6a14e12b42d6052072646b85ba956d2e79541d4f
Daniela Isabel Bolivar Garcia <daniela.bolivar@globant.com>
2020-10-19 10:13:19 -0500

    routing, heroes and about components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Routes
 import { AppRoutingModule } from './app-routing.module';
+
+//Services
+
+//Components
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
